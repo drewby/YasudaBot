c269fb39e3859d1c9ef8356a9715dbd0b3ecbc80
chomado <chomado@chomabook-2.local>
2016-07-29 10:41:17 -0700

    3 screens

---
+++ src/YasudaClient/test160728/ChatPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace test160728
+{
+    public partial class ChatPage : ContentPage
+    {
+        public ChatPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
